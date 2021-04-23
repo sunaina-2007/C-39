410cefd638ddc4964102940f3e7ab4d3c56ce369
Your Name <sunaina-2007>
2021-04-23 17:31:42 +0530

    successfully added

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1_img);
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2_img);
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3_img);
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4_img);
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +46,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background(rgb(198,135,103));
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5);
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -79,6 +85,13 @@ class Game {
       player.update();
     }
 
+    if(player.distance> 3860){
+    gameState = 2;  
+    }
     drawSprites();
+
+  }
+  end(){
+  console.log("gameEnded");  
   }
 }
